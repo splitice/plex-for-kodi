215eafe2729afc1d6b07d9fd3b14698fad4f51c8
Mathew Heard <splitice@users.noreply.github.com>
2020-03-01 12:29:58 +1100

    ...

--- lib/_included_packages/plexnet/plexplayer.py
+++ lib/_included_packages/plexnet/plexplayer.py
@@ -428,10 +428,10 @@ class PlexPlayer(object):
         obj.isTranscoded = True
 
         # if server.supportsFeature("mkvTranscode") and self.item.settings.getPreference("transcode_format", 'mkv') != "hls":
-        if server.supportsFeature("mkvTranscode"):
-            builder = self.buildTranscodeMkv(obj)
-        else:
-            builder = self.buildTranscodeHls(obj)
+        #if server.supportsFeature("mkvTranscode"):
+        #    builder = self.buildTranscodeMkv(obj)
+        #else:
+        builder = self.buildTranscodeHls(obj)
 
         if self.item.getServer().TYPE == 'MYPLEXSERVER':
             path = server.swizzleUrl(self.item.getAbsolutePath("key"))
